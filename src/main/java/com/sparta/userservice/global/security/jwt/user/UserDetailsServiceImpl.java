b55e49b4e0bf0465e54100c5b4ed165b3c9fa6e6
XXN <jaechan27.0.2.28@gmail.com>
2025-11-07 12:28:28 +0900

    feat(user): 로그인 시도 시 승인 여부 검증

--- src/main/java/com/sparta/userservice/global/security/jwt/user/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/userservice/global/security/jwt/user/UserDetailsServiceImpl.java
@@ -1,15 +1,20 @@
 package com.sparta.userservice.global.security.jwt.user;
 
-import com.sparta.userservice.global.exception.UserException;
+import com.sparta.userservice.domain.User;
+import com.sparta.userservice.global.exception.AuthException;
 import com.sparta.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import static com.sparta.userservice.global.response.ErrorCode.USER_NOT_FOUND;
+import static com.sparta.userservice.domain.UserStatus.APPROVE;
+import static com.sparta.userservice.global.response.ErrorCode.AUTH_PENDING_APPROVAL;
+import static com.sparta.userservice.global.response.ErrorCode.AUTH_NOT_FOUND;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -18,8 +23,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
-                .map(UserDetailsImpl::new)
-                .orElseThrow(() -> new UserException(USER_NOT_FOUND));
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new AuthException(AUTH_NOT_FOUND));
+
+        if (!user.getStatus().equals(APPROVE)) {
+            log.error("승인 대기중: status={}", user.getStatus());
+            throw new AuthException(AUTH_PENDING_APPROVAL);
+        }
+
+        return new UserDetailsImpl(user);
     }
 }
